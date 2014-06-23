db8fae78d9e63ee937c1cea1940f52c494509c2e
perfectdaemon <perfectdaemon@gmail.com>
2014-06-23 22:47:36 +0700

    + template project

---
+++ src/demos/_template/template.lpr
@@ -0,0 +1,29 @@
+program template;
+
+uses
+  glrMath, tinyglr,
+  uGame;
+
+var
+  InitParams: TglrInitParams;
+
+begin
+  with InitParams do
+  begin
+    Width := 1000;
+    Height := 600;
+    X := 100;
+    Y := 100;
+    Caption := 'Template project [tiny-glr ' + TINYGLR_VERSION + ']';
+    vSync := True;
+    PackFilesPath := '';
+    UseDefaultAssets := True;
+  end;
+
+  Game := TGame.Create();
+  Core.Init(Game, InitParams);
+  Core.Loop();
+  Core.DeInit();
+  Game.Free();
+end.
+
---
+++ src/demos/_template/ugame.pas
@@ -0,0 +1,76 @@
+unit uGame;
+
+interface
+
+uses
+  tinyglr;
+
+type
+
+  { TGame }
+
+  TGame = class (TglrGame)
+
+  public
+    procedure OnFinish; override;
+    procedure OnInput(aType: TglrInputType; aKey: TglrKey; X, Y,
+      aOtherParam: Integer); override;
+    procedure OnPause; override;
+    procedure OnRender; override;
+    procedure OnResize(aNewWidth, aNewHeight: Integer); override;
+    procedure OnResume; override;
+    procedure OnStart; override;
+    procedure OnUpdate(const dt: Double); override;
+  end;
+
+var
+  Game: TGame;
+
+implementation
+
+{ TGame }
+
+procedure TGame.OnStart;
+begin
+  // Write here initialization code
+end;
+
+procedure TGame.OnFinish;
+begin
+  // Write here code for destroying all of your objects
+end;
+
+procedure TGame.OnInput(aType: TglrInputType; aKey: TglrKey; X, Y,
+  aOtherParam: Integer);
+begin
+  // Calls when engine receives some input info
+end;
+
+procedure TGame.OnUpdate(const dt: Double);
+begin
+  // Place here game logic code
+end;
+
+procedure TGame.OnRender;
+begin
+  // It calls on every draw
+end;
+
+procedure TGame.OnPause;
+begin
+  // Calls when engine receives that app was lost focus
+end;
+
+procedure TGame.OnResume;
+begin
+  // Calls when engine receives that app was focused
+end;
+
+procedure TGame.OnResize(aNewWidth, aNewHeight: Integer);
+begin
+  //Calls when windows has chagned size
+end;
+
+
+end.
+
